96277afd5cf0c3c76d5afd1a0a264a97dbba290c
Umut Zenger <umut.zenger@popcore.com>
2022-10-19 15:58:37 +0200

    whew

--- index.js
+++ index.js
@@ -4,21 +4,14 @@ const github = require("@actions/github");
 function main() {
     try {
         const token = process.env["token"];
-        console.log(token);
         const PAT = process.env["PAT"];
-        console.log(PAT);
         
         const reviewers = core.getInput("reviewers");
-        console.log(reviewers);
         const reviewer_teams = core.getInput("reviewer-teams");
-        console.log(reviewer_teams);
         const reviewerList = reviewers.split("|");
         const teamList = reviewer_teams.split("|");
         
-        const addsTeams = teamList.length > 0;
-        console.log(teamList.length);
-        console.log(addsTeams);
-        const octokit = new github.getOctokit(token);
+        const octokit = new github.getOctokit(PAT == "" ? token : PAT); //prefer PAT
 
         const context = github.context;
         const prNumber = context.payload.pull_request.number;
